10fd09c50c766b574a9295d81ee08e30f7ed7223
David Fraser <davidf@sjsoft.com>
2011-09-23 16:13:10 +0200

    Collect timing results

--- examples/microbench/unitbench.py
+++ examples/microbench/unitbench.py
@@ -1,12 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from unittest import *
+import unittest.case
+import unittest.main
+import unittest.result
+import unittest.runner
+import unittest.suite
+import time
+import sys
 
-class BenchCase(TestCase):
+orig_stdout = sys.stdout
+
+class TextBenchResult(unittest.runner.TextTestResult):
+    """Collects results including benchmarking time"""
+    def startTest(self, test):
+        super(TextBenchResult, self).startTest(test)
+        self.start_time = time.time()
+
+    def stopTest(self, test):
+        """Called when the given test has been run"""
+        self.end_time = time.time()
+        self.stream.writeln("Duration %0.2f" % (self.end_time - self.start_time))
+        super(TextBenchResult, self).stopTest(test)
+
+class BenchCase(unittest.case.TestCase):
     BENCH_REPEATS = 1000
 
-class BenchSuite(TestSuite):
+class BenchSuite(unittest.suite.TestSuite):
     pass
 
+class TextBenchRunner(unittest.runner.TextTestRunner):
+    resultclass = TextBenchResult
 
-    
+def main(*args, **kwargs):
+    kwargs.setdefault('testRunner', TextBenchRunner)
+    kwargs.setdefault('verbosity', 2)
+    unittest.main(*args, **kwargs)
+
