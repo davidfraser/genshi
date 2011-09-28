8ef5605278a7868d0bda9532a7cd4cf31c86e2e9
David Fraser <davidf@sjsoft.com>
2011-09-28 09:38:40 +0200

    Added some more examples

--- genshi/tree/tests/template.py
+++ genshi/tree/tests/template.py
@@ -26,7 +26,7 @@ source3 = """<div xmlns:py="http://genshi.edgewall.org/">
 </div>"""
 
 source4 = """<div xmlns:py="http://genshi.edgewall.org/">
-<span py:for="a in range(5)">Test <i>${a}</i><b py:if="a">Greater than 0</b></span>
+<span py:for="a in range(5)">Test <i>${a}</i><b py:if="a">Greater than 0</b></span>You
 <a py:strip="True">me</a>
 <?python
 print 'Me'
@@ -40,7 +40,26 @@ source5 = """<div xmlns:py="http://genshi.edgewall.org/">
 <span py:for="a in range(5)">Test <i>${a}</i></span>
 </div>"""
 
-source = source2
+source6 = """<div xmlns:py="http://genshi.edgewall.org/">
+Z<a py:strip="True">me</a>X<a py:strip="False">you</a>Y
+A<?python x = 3 ?>B
+<a py:strip="True">me</a>Q
+<?python
+print 'Me'
+?>C
+<py:if test="True"><b>Me</b>Y</py:if>Z
+</div>"""
+
+source7 = """<div xmlns:py="http://genshi.edgewall.org/">
+<a py:strip="True">me</a>
+<?python
+print 'Me'
+?> C
+
+<b>Me</b>
+</div>"""
+
+source = source7
 print "TEMPLATE:"
 print source
 print
