1c07d73d299599bfae472dd0836e91847d7f0772
David Fraser <davidf@sjsoft.com>
2011-10-05 09:48:36 +0200

    Moved out the interpolation code into its own file

--- genshi/tree/template.py
+++ genshi/tree/template.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from genshi.template import base
-from genshi.template import interpolation
 from genshi.template import eval as template_eval
 from genshi.tree import base as tree_base
 from genshi.tree import directives as tree_directives
+from genshi.tree import interpolation
 from genshi.template import markup
 from genshi import core
 from lxml import etree
@@ -12,108 +12,7 @@ import re
 import types
 import collections
 
-class InterpolationString(object):
-    _cache = {}
-    def __new__(cls, text):
-        if text in cls._cache:
-            return cls._cache[text]
-        cls._cache[text] = obj = super(InterpolationString, cls).__new__(cls, text)
-        return obj
-
-    def __init__(self, text):
-        """Constructs an object which will any special variables in the given text"""
-        start = 0
-        self.parts = []
-        while True:
-            m = tree_base.interpolation_re.search(text, start)
-            if m:
-                new_start, end = m.span()
-                self.parts.append(text[start:new_start])
-                expression, varname = m.groups()
-                expression = varname if expression is None else expression
-                if expression is not None:
-                    expr = template_eval.Expression(expression)
-                    self.parts.append(expr)
-                else:
-                    # escaped prefix
-                    self.parts.append(interpolation.PREFIX)
-                start = end
-            else:
-                self.parts.append(text[start:])
-                break
-
-    def eval_expr(self, expr, ctxt, vars):
-        """Returns the result of an expression"""
-        result = base._eval_expr(expr, ctxt, vars)
-        if isinstance(result, (int, float, long)):
-            result = unicode(result)
-        return result
-
-    def interpolate(self, template, ctxt, vars):
-        all_strings = True
-        parts = []
-        for item in self.parts:
-            if isinstance(item, template_eval.Expression):
-                item = self.eval_expr(item, ctxt, vars)
-                if isinstance(item, (list, types.GeneratorType)):
-                    item = tree_base.flatten_generate(item, template, ctxt, vars)
-                    all_strings = False
-                elif isinstance(item, etree._Element) and all_strings:
-                    all_strings = False
-            parts.append(item)
-        if all_strings:
-            return ''.join(parts)
-        return parts
-
-class ContentElement(tree_base.BaseElement):
-    def _init(self):
-        """Instantiates this element - caches items that'll be used in generation"""
-        super(ContentElement, self)._init()
-        self.dynamic_attrs = [(attr_name, InterpolationString(attr_value)) for attr_name, attr_value in self.items() if tree_base.interpolation_re.search(attr_value)]
-        self.static_attrs = dict([(attr_name, attr_value) for attr_name, attr_value in self.items() if attr_name not in self.dynamic_attrs])
-        self.text_dynamic = InterpolationString(self.text) if tree_base.interpolation_re.search(self.text or '') else False
-        self.tail_dynamic = InterpolationString(self.tail) if tree_base.interpolation_re.search(self.tail or '') else False
-
-    def interpolate_attrs(self, template, ctxt, vars):
-        new_attrib = self.static_attrs.copy()
-        for attr_name, attr_value in self.dynamic_attrs:
-            new_attrib[attr_name] = attr_value.interpolate(template, ctxt, vars)
-        # since we're not copying for directives now, but generating, the target needn't be a ContentElement
-        new_attrib[tree_base.LOOKUP_CLASS_TAG] = 'BaseElement'
-        return new_attrib
-
-    def generate(self, template, ctxt, **vars):
-        """Generates XML from this element. returns an Element, an iterable set of elements, or None"""
-        new_element = parser.makeelement(self.tag, self.interpolate_attrs(template, ctxt, vars), self.nsmap)
-        if self.text_dynamic:
-            new_element.text = self.text_dynamic.interpolate(template, ctxt, vars)
-        else:
-            new_element.text = self.text
-        for item in self:
-            new_item = item.generate(template, ctxt, **vars)
-            if not isinstance(new_item, (list, types.GeneratorType)):
-                new_item = [new_item]
-            for sub_item in tree_base.flatten_generate(new_item, template, ctxt, vars):
-                if sub_item is None:
-                    continue
-                elif isinstance(sub_item, etree._Element):
-                    new_element.append(sub_item)
-                elif isinstance(sub_item, basestring):
-                    if len(new_element) == 0:
-                        new_element.text = (new_element.text or "") + sub_item
-                    else:
-                        new_element[-1].tail = (new_element[-1].tail or "") + sub_item
-                else:
-                    raise ValueError("Unexpected type %s returned from generate: %r" % (type(sub_item), sub_item))
-        if self.tail_dynamic:
-            new_element.tail = self.tail_dynamic.interpolate(template, ctxt, vars)
-        else:
-            new_element.tail = self.tail
-        return new_element
-
-tree_base.LOOKUP_CLASSES["ContentElement"] = ContentElement
-
-class DirectiveElement(ContentElement):
+class DirectiveElement(interpolation.ContentElement):
     def _init(self):
         """Instantiates this element - caches items that'll be used in generation"""
         super(DirectiveElement, self)._init()
@@ -162,7 +61,7 @@ class DirectiveElement(ContentElement):
                 final.append(item.generate(template, ctxt, **vars))
             elif isinstance(item, template_eval.Expression):
                 final.append(self.eval_expr(item, ctxt, vars))
-            elif isinstance(item, InterpolationString):
+            elif isinstance(item, interpolation.InterpolationString):
                 final.append(item.interpolate(template, ctxt, vars))
             elif isinstance(item, basestring):
                 # TODO: check if we ever get this rather than an InterpolationString
@@ -211,9 +110,9 @@ class GenshiElementClassLookup(etree.PythonElementClassLookup):
             return DirectiveElement
         for key, value in element.items():
             if tree_base.interpolation_re.search(value):
-                return ContentElement
+                return interpolation.ContentElement
         if (element.text and tree_base.interpolation_re.search(element.text)) or (element.tail and tree_base.interpolation_re.search(element.tail)):
-            return ContentElement
+            return interpolation.ContentElement
         return tree_base.BaseElement
 
 # TODO: find a cleaner way to do this
